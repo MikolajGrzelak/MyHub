03399efae5f4b4c81387af6025f1bb88d9856779
MikolajGrzelak <mikolaj.grzelak@paycomplete.com>
2026-09-24 10:05:37 +0200

    Refresh PWA cache strategy

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,13 +1,30 @@
-const CACHE = "myhub-v1";
+const CACHE = "myhub-v2";
 const APP_SHELL = ["/", "/static/css/app.css", "/static/manifest.json"];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
