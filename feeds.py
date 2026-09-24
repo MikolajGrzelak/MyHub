3c14a558ed2339fa73ab52256a212192bea84699
MikolajGrzelak <mikolaj.grzelak@paycomplete.com>
2026-09-24 10:02:58 +0200

    Add initial RSS source configuration

---
+++ feeds.py
@@ -0,0 +1,12 @@
+FEEDS = [
+    {
+        "name": "Ars Technica",
+        "url": "https://feeds.arstechnica.com/arstechnica/index",
+        "category": "Tech",
+    },
+    {
+        "name": "PC Gamer",
+        "url": "https://www.pcgamer.com/rss/",
+        "category": "Gaming",
+    },
+]
